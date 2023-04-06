448010a5357a8570b9f8c59b429a3337eaac9c11
amanAgrawal-coditas <aman.agrawal@coditas.com>
2023-04-06 18:09:56 +0530

    Proceeded work today on 6th April.

---
+++ src/main/java/com/springboot/blog/Entity/Role.java
@@ -0,0 +1,23 @@
+package com.springboot.blog.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Role
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/springboot/blog/Entity/User.java
@@ -0,0 +1,32 @@
+package com.springboot.blog.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String userName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER,cascade = CascadeType.ALL)//will fetch the roles too
+    @JoinTable(name = "users_roles",joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
+    private Set<Role>roles;
+
+}
