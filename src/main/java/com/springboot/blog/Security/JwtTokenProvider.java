ca284dfdf245e1a30395caed2d11f89b8efb4047
Aman Agrawal <amanagrawal216@gmail.com>
2023-04-13 02:10:33 +0530

    Jwt added

--- src/main/java/com/springboot/blog/Security/JwtTokenProvider.java
+++ src/main/java/com/springboot/blog/Security/JwtTokenProvider.java
@@ -1,10 +1,14 @@
 package com.springboot.blog.Security;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -22,6 +26,27 @@ public class JwtTokenProvider
         String username=authentication.getName();
         Date currentDate=new Date();
         Date expireDate=new Date(currentDate.getTime()+jwtExpirationDate);
-        return "Hiii";
+        String jwtToken;
+        jwtToken = Jwts.builder().setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(expireDate)
+                .signWith(key())
+                .compact();
+        return jwtToken;
+    }
+    private Key key()
+    {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+    public String getUsername(String token)
+    {
+        Claims claims=Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJwt(token).getBody();
+        String username= claims.getSubject();
+        return username;
+    }
+    public Boolean validateToken(String token)
+    {
+        Jwts.parserBuilder().setSigningKey(key()).build().parse(token);
+        return true;
     }
 }
